2dd54cc81055e8a2d532f7a0b7772f18dac3b239
chew1632 <57161389+chew1632@users.noreply.github.com>
2021-05-13 23:43:27 +0800

    Hero detail component
    without changing parent page, hero detail function at the new component.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,17 +11,13 @@ export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
   constructor() { }
 
   ngOnInit() {
   }
 
   selectedHero?: Hero;
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
